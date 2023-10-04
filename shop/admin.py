354da9e40e5f56163ddb1517c33c43e8f9b7f55f
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-10-04 14:20:06 +0545

    Push

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Sub_category, order
+from .models import Product, Category, Sub_category, order, OrderUpdate
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Sub_category)
 admin.site.register(order)
+admin.site.register(OrderUpdate)
