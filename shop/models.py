85d585ecabb1ca0a6fc06f4a4f007898ff5f8ebe
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-10-04 14:22:51 +0545

    Push

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -47,3 +48,13 @@ class order(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default='')
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:7] + "..."
