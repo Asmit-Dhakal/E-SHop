727aa6b8ee9de87241f9e65dc77d65549603eb8e
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-10-08 17:30:03 +0545

    Push

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
+import random
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Product, order, OrderUpdate
+from .models import Product, order, OrderUpdate, Category, Sub_category
 from users.models import Contact
 import json
 from django.views.decorators.csrf import csrf_exempt
@@ -9,13 +10,40 @@ from django.core.paginator import Paginator
 
 
 def index(request):
-    product_objects = Product.objects.all()
-    # search code
-    item_name = request.GET.get('item_name')
-    if item_name != '' and item_name is not None:
-        product_objects = product_objects.filter(name__contains=item_name)
+    categories = Category.objects.all()
+    subcategories = Sub_category.objects.all()
 
-    return render(request, 'shop/index.html', {'product_objects': product_objects})
+    selected_category_id = request.GET.get('category_id')
+    selected_subcategory_id = request.GET.get('subcategory_id')
+
+    # Filter products based on selected category and subcategory
+    product_objects = Product.objects.all()
+
+    if selected_category_id:
+        product_objects = product_objects.filter(category_id=selected_category_id)
+
+    if selected_subcategory_id:
+        product_objects = product_objects.filter(sub_category_id=selected_subcategory_id)
+
+    # Search code
+    item_name = request.GET.get('item_name')
+    if item_name:
+        product_objects = product_objects.filter(name__icontains=item_name)
+
+    # If no category or subcategory is selected, select a random subset of products
+    if not selected_category_id and not selected_subcategory_id:
+        random_products = random.sample(list(product_objects), min(5, len(product_objects)))
+    else:
+        random_products = []
+
+    return render(request, 'shop/index.html', {
+        'product_objects': product_objects,
+        'categories': categories,
+        'subcategories': subcategories,
+        'selected_category_id': int(selected_category_id) if selected_category_id else None,
+        'selected_subcategory_id': int(selected_subcategory_id) if selected_subcategory_id else None,
+        'random_products': random_products,
+    })
 
 
 def detail(request, id):
@@ -52,12 +80,13 @@ def tracker(request):
                 updates = []
                 for item in update:
                     updates.append({'text': item.update_desc, 'time': item.timestamp})
-                    response = json.dumps([updates, orders[0].item_json], default=str)
+                    response = json.dumps({"status": "success", "updates": updates, "itemsJson": orders[0].item_json},
+                                          default=str)
                 return HttpResponse(response)
             else:
-                return HttpResponse('{}')
+                return HttpResponse('{"status":"no item"}')
         except Exception as e:
-            return HttpResponse('{}')
+            return HttpResponse('{"status":"error"}')
 
     return render(request, 'shop/tracking.html')
 
@@ -84,7 +113,7 @@ def checkout(request):
         update.save()
         thank = True
         id = orders.order_id
-    # return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
+        return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
     # ---- transfer amount to paytm
     return render(request, 'shop/checkout.html')
 
